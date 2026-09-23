97349f7f2a04cf413428662776a90016cb0fdb05
TATSUNO “Taz” Yasuhiro <ytatsuno.jp@gmail.com>
2026-09-23 13:00:32 +0900

    Test that literal strings and keys take non-ASCII text

--- cli-java/src/test/java/net/exoego/uika/cli/TomlTest.java
+++ cli-java/src/test/java/net/exoego/uika/cli/TomlTest.java
@@ -85,6 +85,13 @@ class TomlTest {
     }
 
     @Test
+    void literalStringsTakeNonAscii() {
+        Toml.Table t = Toml.parse("'kéy' = 'é 日本語 𝄞'\nml = '''\nü\n'''\n");
+        assertEquals("é 日本語 𝄞", string(t, "kéy"));
+        assertEquals("ü\n", string(t, "ml"));
+    }
+
+    @Test
     void multiLineBasicStrings() {
         Toml.Table t = Toml.parse("a = \"\"\"\nfirst newline is trimmed\nsecond stays\"\"\"\n"
                 + "b = \"\"\"line \\\n      continued \\  \n\n   here\"\"\"\n"
